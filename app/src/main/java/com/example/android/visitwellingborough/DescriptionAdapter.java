cbb1ab0002e7e5f5e42d7257cde75b1ed64a4add
Ian Welsh <derekandclive1@googlemail.com>
2017-06-27 21:54:46 +0100

    Fourth commit. Made the required changes: indentation corrected; redundant activities deleted.

--- app/src/main/java/com/example/android/visitwellingborough/DescriptionAdapter.java
+++ app/src/main/java/com/example/android/visitwellingborough/DescriptionAdapter.java
@@ -1,14 +1,14 @@
 package com.example.android.visitwellingborough;
 
-        import android.app.Activity;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 /**
  * Created by Kezia on 18/06/2017.
